53032297576b86d1c7aeae599648391c75007ca0
theSLWayne <hiranhasanka@gmail.com>
2022-02-07 00:13:29 +0530

    Add blur_faces.py

---
+++ blur_faces.py
@@ -0,0 +1,18 @@
+"""
+Entrypoint for face blurring process
+"""
+
+import argparse
+import glog as logger
+
+from tools import blurredFrames, extractAudio, extractMetaData
+from moviepy.video.io import ImageSequenceClip
+
+def initArgs():
+    pass
+
+def getFileName():
+    pass
+
+def blurVideoFaces():
+    pass
