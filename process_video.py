b3a36e482d918c3ee287c2e3200795b82516b0ad
theSLWayne <hiranhasanka@gmail.com>
2022-02-06 23:38:24 +0530

    Video Processor: function to get blurred frames

--- process_video.py
+++ process_video.py
@@ -8,12 +8,11 @@ Input video processing that includes:
 
 import cv2
 
-def printProgressBar (iteration, total, prefix = 'Progress', suffix = 'Complete', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+def printProgressBar(iteration, total, prefix = 'Progress', suffix = 'Complete', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
     """
     Terminal progress bar function
     Thanks to Greenstick & Diogo of Stackoverflow (https://stackoverflow.com/a/34325723/2817832) for the function. 
 
-    @params:
     :param iteration : current iteration (Int)
     :param total     : total iterations (Int)
     :param prefix    : prefix string (Str)
@@ -41,5 +40,73 @@ def convertToRGB(img):
     """
 
     return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+def blurredFrames(vid_path, frame_count, show=False):
+
+    """
+    Return chronological list of frames with blurred faces
+
+    :param vid_path: Path to the video
+    :param frame_count: Total number of frames in the input video file
+    :param show: Whether the frames should be shown as they are processed
+    :return: List of frames with blurred faces in chronological order
+    """
+
+    # Frame counter
+    frames = 0
+
+    # Blurred Frames list
+    blurred_frames = []
+
+    # Load video
+    vid = cv2.VideoCapture(vid_path)
+
+    # Loop till the end of the video
+    while(vid.isOpened()):
+        ret, frame = vid.read()
+
+        # Convert frame to grayscale to make it easier for face detection
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # Haar Cascade model to detect faces
+        haar_face_casscade = cv2.CascadeClassifier('cascade_files/haarcascade_frontalface_alt.xml')
+
+        # Detect faces
+        faces = haar_face_casscade.detectMultiScale(gray_frame, scaleFactor=1.1, minNeighbors=4)
+
+        # Loop through all faces detected
+        for (x, y, w, h) in faces:
+
+            # Apply blur to detected face
+            roi = frame[y:y+h, x:x+w]
+            roi = cv2.GaussianBlur(roi, (43, 43), 30)
+            frame[y:y+roi.shape[0], x:x+roi.shape[1]] = roi
+
+        # Append blurred frames to the list
+        blurred_frames.append(convertToRGB(frame))
+
+        # Display current frame - after blurring
+        if show:
+            cv2.imshow('Frame', frame)
+
+        # Update progressbar
+        frames += 1
+        printProgressBar(frames, frame_count)
+
+        # Break loop if all frames are iterated through
+        if frames == frame_count:
+            break
+
+        # Define "q" as the exit button to stop the process
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+
+    # Release the loaded video object
+    vid.release()
+
+    # Close all windows opened to display frames
+    cv2.destroyAllWindows()
+
+    return blurred_frames
 
 
